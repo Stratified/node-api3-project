e1bc72f2211a9939f8cfb7ddf209fa8cabd849d0
Brian Bagrov <Brianbagrov@gmail.com>
2020-10-06 22:17:17 -0400

    MVP reached.

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,25 @@
-const express = require('express');
-
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+	res.send(`<h1>postRouter</h1>`);
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+	// do your magic!
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+	// do your magic!
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+	// do your magic!
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+	// do your magic!
 }
 
 module.exports = router;
